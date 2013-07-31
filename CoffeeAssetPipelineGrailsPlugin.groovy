6f3a1e053b5976a0cbdf3ddbc2f898915f590831
David Estes <davydotcom@gmail.com>
2013-07-31 14:25:24 -0400

    Renamed plugin

--- CoffeeAssetPipelineGrailsPlugin.groovy
+++ CoffeeAssetPipelineGrailsPlugin.groovy
@@ -1,19 +1,19 @@
 import asset.pipeline.AssetHelper
 import asset.pipeline.coffee.CoffeeAssetFile
 
-class CoffeeGrailsGrailsPlugin {
+class CoffeeAssetPipelineGrailsPlugin {
     def version = "0.1"
     def grailsVersion = "2.0 > *"
-    def title = "CoffeeScript Grails Plugin"
+    def title = "CoffeeScript Asset-Pipeline Plugin"
     def author = "David Estes"
     def authorEmail = "destes@bcap.com"
     def description = "Provides coffee-script support for the asset-pipeline static asset management plugin."
-    def documentation = "http://github.com/bertramdev/coffee-grails"
+    def documentation = "http://github.com/bertramdev/coffee-grails-asset-pipeline"
 
     def license = "APACHE"
     def organization = [ name: "Bertram Capital", url: "http://www.bertramcapital.com/" ]
-    def issueManagement = [ system: "GITHUB", url: "http://github.com/bertramdev/coffee-grails/issues" ]
-    def scm = [ url: "http://github.com/bertramdev/coffee-grails" ]
+    def issueManagement = [ system: "GITHUB", url: "http://github.com/bertramdev/coffee-grails-asset-pipeline/issues" ]
+    def scm = [ url: "http://github.com/bertramdev/coffee-grails-asset-pipeline" ]
 
     def doWithDynamicMethods = { ctx ->
         AssetHelper.assetSpecs << CoffeeAssetFile
