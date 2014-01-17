ec7de80bb80d020b3d1f37d1f30b2908d6c329b4
David Estes <davydotcom@gmail.com>
2014-01-17 18:20:47 -0500

    updating to 1.5 stack

--- src/groovy/asset/pipeline/coffee/CoffeeAssetFile.groovy
+++ src/groovy/asset/pipeline/coffee/CoffeeAssetFile.groovy
@@ -1,49 +1,15 @@
 package asset.pipeline.coffee
 import asset.pipeline.CacheManager
+import asset.pipeline.AbstractAssetFile
 import asset.pipeline.AssetHelper
 
-class CoffeeAssetFile {
+class CoffeeAssetFile extends AbstractAssetFile {
 	static final contentType = ['application/javascript','application/x-javascript','text/javascript']
 	static extensions = ['coffee', 'js.coffee']
 	static final String compiledExtension = 'js'
 	static processors = [CoffeeScriptProcessor]
 
-	File file
-	def baseFile
-	def encoding
-
-	CoffeeAssetFile(file, baseFile=null) {
-		this.file = file
-		this.baseFile = baseFile
-	}
-
-	def processedStream(precompiler=false) {
-		def fileText
-		if(baseFile?.encoding || encoding) {
-			fileText = file?.getText(baseFile?.encoding ? baseFile.encoding : encoding)
-		} else {
-			fileText = file?.text
-		}
-
-		def md5 = AssetHelper.getByteDigest(fileText.bytes)
-		if(!precompiler) {
-			def cache = CacheManager.findCache(file.canonicalPath, md5)
-			if(cache) {
-				return cache
-			}
-		}
-		for(processor in processors) {
-			def processInstance = processor.newInstance(precompiler)
-			fileText = processInstance.process(fileText, this)
-		}
-		if(!precompiler) {
-			CacheManager.createCache(file.canonicalPath,md5,fileText)
-		}
-		return fileText
-		// Return File Stream
-	}
-
-	def directiveForLine(line) {
+	String directiveForLine(String line) {
 		line.find(/#=(.*)/) { fullMatch, directive -> return directive }
 	}
 }
