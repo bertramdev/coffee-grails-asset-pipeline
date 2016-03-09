08326b35fa11b0faf999d14a0a1fb131695813c3
David Estes <davydotcom@gmail.com>
2016-03-09 09:50:55 -0500

    Version bump to 2.7.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,11 +11,11 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile 'com.bertramlabs.plugins:coffee-asset-pipeline:2.7.0'
+		compile 'com.bertramlabs.plugins:coffee-asset-pipeline:2.7.2'
 	}
 	plugins {
 
-		runtime ":asset-pipeline:2.7.0"
+		runtime ":asset-pipeline:2.7.2"
 
 		build ':release:3.1.2', ':rest-client-builder:2.0.1', {
             export = false
