3be83f957e4b5aa49886dfedb59f27f1a5bb26f8
David Estes <davydotcom@gmail.com>
2016-08-15 08:42:57 -0400

    updating to latest core coffee-asset-pipeline dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,11 +11,11 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile 'com.bertramlabs.plugins:coffee-asset-pipeline:2.9.1'
+		compile 'com.bertramlabs.plugins:coffee-asset-pipeline:2.11.0'
 	}
 	plugins {
 
-		runtime ":asset-pipeline:2.9.1"
+		runtime ":asset-pipeline:2.11.0"
 
 		build ':release:3.1.2', ':rest-client-builder:2.0.1', {
             export = false
